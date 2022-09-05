1be9012232b010dcefa17a079a42daa90b7093f7
jubamatsaberidze <j.matsaberidze.websmart@gmail.com>
2022-09-05 17:04:53 +0400

    project && client queries

---
+++ server/index.js
@@ -0,0 +1,19 @@
+require('dotenv').config();
+const express = require('express');
+const { graphqlHTTP } = require('express-graphql');
+const schema = require('./schema');
+const connectDB = require('./config/db');
+const colors = require('colors');
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+
+//Connection to database
+connectDB();
+
+app.use('/graphql', graphqlHTTP({
+    schema, 
+    graphiql: process.env.NODE_ENV === 'development'
+}))
+
+app.listen(PORT, console.log(`Server started listening on port ${PORT}...`));
